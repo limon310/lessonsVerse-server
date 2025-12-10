eb959041b50b30ac328fe9ec5be8eb9ce24f07e5
Md.limon islam <mdlimonislam134@gmail.com>
2025-12-10 13:05:44 +0600

    get specific user by email

--- index.js
+++ index.js
@@ -82,6 +82,14 @@ async function run() {
       res.send(result);
     })
 
+    // get user by email
+    app.get('/users/:email', async(req,res) =>{
+      const email = req.params.email;
+      const query = {email};
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+
     // LESSONS RELETADE APIS HERE
     // create lessons
     app.post('/lessons', async (req, res) => {
